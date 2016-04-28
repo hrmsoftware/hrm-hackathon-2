fb4d69932ea8681730cf5c03757add9a86d3267a
Klas Axell <klas.axell@hrmsoftware.se>
2016-04-28 17:17:08 +0200

    Initial rest stuff

--- server/enviroslim/src/main/java/se/hrmsoftware/EnviroslimApplication.java
+++ server/enviroslim/src/main/java/se/hrmsoftware/EnviroslimApplication.java
@@ -1,12 +1,14 @@
 package se.hrmsoftware;
 
+import java.io.IOException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EnviroslimApplication {
-
-	public static void main(String[] args) {
+	
+	public static void main(String[] args) throws IOException {
 		SpringApplication.run(EnviroslimApplication.class, args);
 	}
 }
