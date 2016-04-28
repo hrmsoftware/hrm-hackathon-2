1fab3f1351a04e76f88de50f6806419b6061abe0
Marcus Edvinsson <marcus.edvinsson@hrmsoftware.se>
2016-04-28 22:16:36 +0200

    Smaller changes to the client.

--- client/www/js/recycle_service.js
+++ client/www/js/recycle_service.js
@@ -2,7 +2,7 @@ angular.module('starter')
 .factory('recycleService', function($http) {
 	return {
 		getFuture: function(type) {
-			return $http.get("http://localhost:8080/trashme/future?type=" + type);
+			return $http.get("http://172.20.10.105:8080/trashme/future?type=" + type);
 		}
 	}
 })
