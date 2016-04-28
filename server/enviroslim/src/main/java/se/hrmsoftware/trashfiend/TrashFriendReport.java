03d90f4a99030b8c66fe40504d00f2bc301d57e6
Klas Axell <klas.axell@hrmsoftware.se>
2016-04-28 19:44:48 +0200

    Added busyStatus

--- server/enviroslim/src/main/java/se/hrmsoftware/trashfiend/TrashFriendReport.java
+++ server/enviroslim/src/main/java/se/hrmsoftware/trashfiend/TrashFriendReport.java
@@ -1,7 +1,17 @@
 package se.hrmsoftware.trashfiend;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class TrashFriendReport {
 
+	private final int busyStatus;
 	
+	public TrashFriendReport(int busyStatus) {
+		this.busyStatus = busyStatus;
+	}
 	
+	public int getBusyStatus() {
+		return busyStatus;
+	}
 }
