78015095d53e9b989ec6b9a6606b76cf65ca289c
Martin Moberg <martin.moberg@hrmsoftware.se>
2016-04-28 14:10:20 +0200

    Initial commit

---
+++ server/enviroslim/src/main/java/se/hrmsoftware/EnviroslimApplication.java
@@ -0,0 +1,12 @@
+package se.hrmsoftware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnviroslimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnviroslimApplication.class, args);
+	}
+}
---
+++ server/enviroslim/src/test/java/se/hrmsoftware/EnviroslimApplicationTests.java
@@ -0,0 +1,18 @@
+package se.hrmsoftware;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = EnviroslimApplication.class)
+@WebAppConfiguration
+public class EnviroslimApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
